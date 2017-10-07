6881e500ea2ff81e287e2b074adb25859e072dec
Lando Tanghe <lando.tanghe@gmail.com>
2017-10-07 17:31:55 +0200

    extract StateNamer

--- RegularExpressionEvaluator/RegularExpression.cs
+++ RegularExpressionEvaluator/RegularExpression.cs
@@ -9,8 +9,6 @@ namespace RegularExpressionEvaluator
         private Automaton<string, char> _automaton;
         private Sequence CompleteSequence;
 
-        private const string StartStatePrefix = "Start";
-        private const string FinalStatePrefix = "Final";
 
         internal RegularExpression(Automaton<string, char> automaton, Sequence sequence)
         {
@@ -55,6 +53,7 @@ namespace RegularExpressionEvaluator
 
         internal class RegularExpressionBuilder
         {
+            private StateNamer StateNamer;
             private PatternReader PatternReader;
             private AutomatonBuilder AutomatonBuilder;
             private int stateId = 0;
@@ -63,6 +62,7 @@ namespace RegularExpressionEvaluator
             {
                 PatternReader = new PatternReader(pattern);
                 AutomatonBuilder = new AutomatonBuilder();
+                StateNamer = new StateNamer();
             }            
 
             public RegularExpression Build()
@@ -76,7 +76,7 @@ namespace RegularExpressionEvaluator
             {
                 var sequence = new Sequence
                 {
-                    StartState = StartStatePrefix + stateId++
+                    StartState = StateNamer.CreateNameForStartOfSequence()
                 };
 
                 AutomatonBuilder.State(sequence.StartState).ActiveAtStart();
@@ -86,7 +86,7 @@ namespace RegularExpressionEvaluator
                 {
                     var symbol = PatternReader.ReadNextSymbol();
 
-                    var currentState = "Character " + stateId++;
+                    var currentState = StateNamer.CreateNameForIntermediateState();
 
                     AutomatonBuilder.State(currentState)
                         .Transition().On(symbol).From(previousState).To(currentState);
@@ -94,21 +94,44 @@ namespace RegularExpressionEvaluator
                     previousState = currentState;
                 }
 
-                sequence.FinalState = FinalStatePrefix + stateId++;
+                sequence.EndState = StateNamer.CreateNameForEndOfSequence();
 
-                AutomatonBuilder.State(sequence.FinalState)
-                    .Transition().OnEpsilon().From(previousState).To(sequence.FinalState);
+                AutomatonBuilder.State(sequence.EndState)
+                    .Transition().OnEpsilon().From(previousState).To(sequence.EndState);
 
                 return sequence;
             }
         }
 
+        internal class StateNamer
+        {
+            private const string StartStatePrefix = "Start ";
+            private const string FinalStatePrefix = "Final ";
+            private const string IntermediatePrefix = "Intermediate ";
+
+            private int StateNumber = 0;
+
+            public string CreateNameForStartOfSequence()
+            {
+                return StartStatePrefix + StateNumber++;
+            }
+
+            public string CreateNameForEndOfSequence()
+            {
+                return FinalStatePrefix + StateNumber++;
+            }
+
+            public string CreateNameForIntermediateState()
+            {
+                return IntermediatePrefix + StateNumber++;
+            }
+        }
 
 
         internal class Sequence
         {
             public string StartState { get; set; }
-            public string FinalState { get; set; }
+            public string EndState { get; set; }
         }
 
         
@@ -120,7 +143,7 @@ namespace RegularExpressionEvaluator
                 _automaton.Process(symbol);
             }
 
-            return _automaton.GetActiveStates().Any(state => state.Description == CompleteSequence.FinalState);
+            return _automaton.GetActiveStates().Any(state => state.Description == CompleteSequence.EndState);
         }
     }
 }
